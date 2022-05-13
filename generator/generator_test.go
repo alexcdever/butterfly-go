3361492032617415199505a88fc35b9d7622c268
alexcdever <alexcdever@gmail.com>
2022-05-13 17:42:57 +0000

    add two unit test cases for constructing with timestamp and with old id

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -5,6 +5,44 @@ import (
 	"time"
 )
 
+func TestNewWithTimestamp(t *testing.T) {
+	timestamp := time.Now().UnixMilli()
+	generator, err := NewWithTimestamp(timestamp)
+	if err != nil {
+		t.Errorf("failed to construct generator: %v", err)
+	}
+	if generator.timestamp == timestamp {
+		t.Log("test successfully")
+	} else {
+		t.Errorf("timestamp is different: origin[%d]:generator[%d]", timestamp, generator.timestamp)
+	}
+}
+
+func TestNewWithId(t *testing.T) {
+	generator, err := NewWithTimestamp(time.Now().UnixMilli())
+	if err != nil {
+		t.Errorf("failed to construct generator: %v", err)
+	}
+	id, err := generator.Generate()
+	if err != nil {
+		t.Errorf("failed to generate id: %v", err)
+	}
+	gen2, err := NewWithId(id)
+	if err != nil {
+		t.Errorf("failed to construct the second generator: %v", err)
+	}
+	id2, err := gen2.Generate()
+	if err != nil {
+		t.Errorf("failed to generate id by gen2: %v", err)
+	}
+	if id2 > id {
+		t.Log("test successfully")
+	} else {
+		t.Errorf("the id2[%d] must be bigger than id[%d]", id2, id)
+	}
+
+}
+
 func TestButterfly_Generate(t *testing.T) {
 	count := 20
 	generator, err := NewWithTimestamp(time.Now().UnixMilli())
